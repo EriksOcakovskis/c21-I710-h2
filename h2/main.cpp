c9658dac74077276481d455f7004b9bd0702da95
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2017-12-25 13:09:58 +0200

    Initial Commit

---
+++ h2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  h2
+//
+//  Created by Eriks Ocakovskis on 25/12/2017.
+//  Copyright © 2017 Eriks Ocakovskis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
